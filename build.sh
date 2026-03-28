e515a8d7238f424c183e7f29de460bdb93b6fdfb
keks137 <keks137@proton.me>
2026-03-28 16:38:14 +0100

    include kotlin stdlib

--- build.sh
+++ build.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 set -xe
 
+APP=androtest
 SDK=~/android_sdk
 PLATFORM=$SDK/platforms/android-29
 BUILD_TOOLS=$SDK/build-tools/29.0.3
+KOTLIN_STDLIB=/usr/share/kotlin/lib/kotlin-stdlib.jar
 
-kotlinc src/*.kt -d classes/ \
+
+mkdir -p classes
+kotlinc src/*.kt -d classes/classes.jar \
     -cp $PLATFORM/android.jar \
-    -include-runtime
+
 
 mkdir -p dex
 $BUILD_TOOLS/d8 \
     --lib $PLATFORM/android.jar \
-    classes/bubble/keks/androtest/*.class \
+    classes/classes.jar\
+    $KOTLIN_STDLIB \
     --output dex/
 
 # Package
@@ -26,3 +31,4 @@ zip -uj unaligned-app.apk dex/classes.dex
 
 $BUILD_TOOLS/zipalign -f 4 unaligned-app.apk app.apk
 $BUILD_TOOLS/apksigner sign --ks debug.keystore --ks-pass pass:android app.apk
+
